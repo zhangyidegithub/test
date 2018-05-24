725952e4bc80f10327c06b8d5a1ebaa690b245c1
986256696@qq.com <27430526+zhangyidegithub@users.noreply.github.com>
2018-05-24 18:26:36 +0800

    Revert "利用HttpClient进行HTTPS接口调用 demo"
    This reverts commit 864824653f252060433bb12adb5536da67143779.
    
    Signed-off-by: 986256696@qq.com <27430526+zhangyidegithub@users.noreply.github.com>

--- http/src/com/vdvd.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class vdvd {
-	public static void main(String[] args){ 
-        String url = "";
-        String jsonStr = "{s:ss}";
-        String httpOrgCreateTestRtn = HttpClientUtil.doPost(url, jsonStr, "utf-8");
-    }
-}
